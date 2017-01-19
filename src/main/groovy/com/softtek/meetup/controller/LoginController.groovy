265a84ee68056fdcc68a85ab605f5d09dd663925
josdem <joseluis.delacruz@gmail.com>
2017-01-19 16:32:37 -0600

    #11 Fixing tags in html files

--- src/main/groovy/com/softtek/meetup/controller/LoginController.groovy
+++ src/main/groovy/com/softtek/meetup/controller/LoginController.groovy
@@ -1,23 +1,13 @@
 package com.softtek.meetup.controller
 
-import static org.springframework.web.bind.annotation.RequestMethod.GET
-import static org.springframework.web.bind.annotation.RequestMethod.POST
-
-import javax.validation.Valid 
-
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.InitBinder
-import org.springframework.web.bind.WebDataBinder
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.beans.factory.annotation.Value
-import org.springframework.validation.BindingResult
 import org.springframework.stereotype.Controller
 
 import com.softtek.meetup.service.LocaleService
-import com.softtek.meetup.validator.UserValidator
-import com.softtek.meetup.command.UserCommand
 
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
@@ -26,36 +16,18 @@ import org.slf4j.LoggerFactory
 class LoginController {
 
   @Autowired
-  UserValidator userValidator
-  @Autowired
   LocaleService localeService
 
   Logger log = LoggerFactory.getLogger(this.class)
 
-  @InitBinder
-  private void initBinder(WebDataBinder binder) {
-    binder.addValidators(userValidator)
-  }
-
-  @RequestMapping(method = GET, value = "/login")
+  @RequestMapping("/login")
   ModelAndView login(@RequestParam Optional<String> error){
     log.info "Calling login"
     ModelAndView modelAndView = new ModelAndView('login/login')
-    def userCommand = new UserCommand()
-    modelAndView.addObject('userCommand', userCommand)
     if(error.isPresent()){
       modelAndView.addObject('error', localeService.getMessage('login.error'))
     }
     modelAndView
-  }
-
-  @RequestMapping(method = POST, value = "/save")
-  String save(@Valid UserCommand command, BindingResult bindingResult) {
-    log.info "Saving user"
-    if (bindingResult.hasErrors()) {
-      return 'login'
-    }
-    'redirect:/'
   }
 
 }
