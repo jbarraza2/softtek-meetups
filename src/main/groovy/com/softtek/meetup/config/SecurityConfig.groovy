6f32b88f356572cd52d6954f20f3b2bbfff2a7c5
josdem <joseluis.delacruz@gmail.com>
2017-01-23 15:45:44 -0600

    #8 Changing configuration from memory to database

--- src/main/groovy/com/softtek/meetup/config/SecurityConfig.groovy
+++ src/main/groovy/com/softtek/meetup/config/SecurityConfig.groovy
@@ -2,16 +2,23 @@ package com.softtek.meetup.config
 
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.context.annotation.Configuration
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
+
 
 @Configuration
 @EnableWebSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+  @Autowired
+  UserDetailsService userDetailsService
+
   @Override
-  protected void configure(HttpSecurity http) throws Exception {
+  void configure(HttpSecurity http) throws Exception {
     http
     .authorizeRequests()
     .antMatchers("/", "/assets/**","/home/**","/user/**").permitAll()
@@ -19,6 +26,7 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
     .and()
     .formLogin()
     .loginPage("/login")
+    .usernameParameter("username")
     .permitAll()
     .and()
     .logout()
@@ -28,7 +36,7 @@ class SecurityConfig extends WebSecurityConfigurerAdapter {
   @Autowired
   void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
     auth
-    .inMemoryAuthentication()
-    .withUser("user").password("password").roles("USER")
+    .userDetailsService(userDetailsService)
+    .passwordEncoder(new BCryptPasswordEncoder())
   }
 }
