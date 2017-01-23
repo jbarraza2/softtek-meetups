ed66b1f81e0c17bc6e799aacef71639bde4c39ff
josdem <joseluis.delacruz@gmail.com>
2017-01-23 14:49:54 -0600

    #12 Deleting information from internationalization that we dont need

--- src/main/groovy/com/softtek/meetup/controller/HomeController.groovy
+++ src/main/groovy/com/softtek/meetup/controller/HomeController.groovy
@@ -1,26 +1,13 @@
 package com.softtek.meetup.controller
 
-
 import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.beans.factory.annotation.Value
-import com.softtek.meetup.service.SofttekMeetupService
+import org.springframework.stereotype.Controller
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
-
-import org.springframework.stereotype.Controller
-
-import com.softtek.meetup.service.SofttekMeetupService
 
 @Controller
 class HomeController {
 	Logger log = LoggerFactory.getLogger(this.class)
-
-  @Autowired
-  SofttekMeetupService service
-
-  @Value('${message}')
-  String message
 
   @RequestMapping("/")
   String index(){
