733d8dc7a22aa1050af4db764e0dee00b653bf7d
josdem <joseluis.delacruz@gmail.com>
2017-01-31 16:35:26 -0600

    #42 Agregando las notaciones de persistencia

--- src/main/groovy/com/softtek/meetup/model/RegistrationCode.groovy
+++ src/main/groovy/com/softtek/meetup/model/RegistrationCode.groovy
@@ -1,12 +1,31 @@
 package com.softtek.meetup.model
 
+import static javax.persistence.EnumType.STRING
+import static javax.persistence.GenerationType.AUTO
+
+import java.io.Serializable
+import javax.persistence.Id
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.Enumerated
+import javax.persistence.GeneratedValue
+
 import com.softtek.meetup.enums.RegistrationCodeStatus
 
+@Entity
 class RegistrationCode {
 
-  RegistrationCodeStatus status = RegistrationCodeStatus.VALID;
+  @Id
+  @GeneratedValue(strategy=AUTO)
+  Long id
+  @Column(nullable = false)
+  String email
+  @Column(nullable = false)
 	Date dateCreated = new Date()
+  @Column(nullable = false)
   String token = UUID.randomUUID().toString().replaceAll('-','')
+  @Column(nullable = false)
+  RegistrationCodeStatus status = RegistrationCodeStatus.VALID;
 
   Boolean isValid(){
     status == RegistrationCodeStatus.VALID ? true : false
