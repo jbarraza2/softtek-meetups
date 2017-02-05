39b6f8bbb8854d21b32994c1f63e0ebd2d29bf4e
josdem <joseluis.delacruz@gmail.com>
2017-02-05 10:32:52 -0600

    #37 Fixing RecoveryServiceSpec test

--- src/test/groovy/com/softtek/meetup/unit/RecoveryServiceSpec.groovy
+++ src/test/groovy/com/softtek/meetup/unit/RecoveryServiceSpec.groovy
@@ -3,27 +3,32 @@ package com.softtek.meetup.unit
 import spock.lang.Specification
 
 import com.softtek.meetup.service.RecoveryService
+import com.softtek.meetup.service.RestService
 import com.softtek.meetup.service.impl.RecoveryServiceImpl
 import com.softtek.meetup.repository.RegistrationCodeRepository
 import com.softtek.meetup.model.RegistrationCode
+import com.softtek.meetup.command.Command
 
 class RecoveryServiceSpec extends Specification {
 
   RecoveryService recoveryService = new RecoveryServiceImpl()
 
   RegistrationCodeRepository repository = Mock(RegistrationCodeRepository)
+  RestService restService = Mock(RestService)
 
   def setup(){
     recoveryService.repository = repository
+    recoveryService.restService = restService
   }
 
 	void "should persist an registration code"(){
     given:"An email"
       String email = 'josdem@email.com'
     when:"We call regisgtration code"  
-      recoveryService.saveRegistrationCode(email)
+      recoveryService.sendConfirmationAccountToken(email)
     then:"We expect to call save from repository"
     1 * repository.save(_ as RegistrationCode)  
+    1 * restService.sendCommand(_ as Command)  
   }
 	
 }
