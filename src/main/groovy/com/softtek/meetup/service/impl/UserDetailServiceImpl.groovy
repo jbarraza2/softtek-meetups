970c40d37a55949954fc315796a10fa16d3dbf26
josdem <joseluis.delacruz@gmail.com>
2017-02-06 16:49:14 -0600

    Fixing username method name

--- src/main/groovy/com/softtek/meetup/service/impl/UserDetailServiceImpl.groovy
+++ src/main/groovy/com/softtek/meetup/service/impl/UserDetailServiceImpl.groovy
@@ -19,7 +19,7 @@ class UserDetailServiceImpl implements UserDetailsService {
 
   @Override
   org.springframework.security.core.userdetails.User loadUserByUsername(String username) throws UsernameNotFoundException {
-    User user = userService.getUserByUsername(username)
+    User user = userService.getByUsername(username)
     Set<GrantedAuthority> grantedAuthorities = [] as Set
     grantedAuthorities << new SimpleGrantedAuthority(user.role.toString())
     new org.springframework.security.core.userdetails.User(user.username, user.password, grantedAuthorities)
