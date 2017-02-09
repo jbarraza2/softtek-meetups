be2075d271711256911f50aef55f24914872bf85
josdem <joseluis.delacruz@gmail.com>
2017-02-09 15:03:35 -0600

    #39 Adding confirmAccountForToken implementation

--- src/main/groovy/com/softtek/meetup/service/impl/RecoveryServiceImpl.groovy
+++ src/main/groovy/com/softtek/meetup/service/impl/RecoveryServiceImpl.groovy
@@ -8,6 +8,7 @@ import com.softtek.meetup.service.RecoveryService
 import com.softtek.meetup.service.RestService
 import com.softtek.meetup.repository.RegistrationCodeRepository
 import com.softtek.meetup.model.RegistrationCode
+import com.softtek.meetup.model.User
 import com.softtek.meetup.command.Command
 import com.softtek.meetup.command.MessageCommand
 
@@ -29,6 +30,21 @@ class RecoveryServiceImpl implements RecoveryService {
     repository.save(registrationCode)
     Command command = new MessageCommand(email:email, template:template, url:"${serverName}${registrationCode.token}")
     restService.sendCommand(command)
+  }
+
+   User confirmAccountForToken(String token){
+    User user = getUserByToken(token)
+    if(!user) throw new UserNotFoundException(localeService.getMessage('exception.user.not.found'))
+    user.enabled = true
+    userRepository.save(user)
+    user
+  }
+
+  User getUserByToken(String token){
+    String email = registrationService.findEmailByToken(token)
+    if(!email) throw new VetlogException(localeService.getMessage('exception.token.not.found'))
+    User user = userRepository.findByEmail(email)
+    user
   }
 
 }
