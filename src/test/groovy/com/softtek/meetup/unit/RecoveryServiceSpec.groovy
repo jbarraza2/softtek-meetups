07e971954208d73a3c30e744f66b41df7604de53
josdem <joseluis.delacruz@gmail.com>
2017-02-10 16:14:42 -0600

    #49 Agregando el test para validar que se confirma una cuenta por token

--- src/test/groovy/com/softtek/meetup/unit/RecoveryServiceSpec.groovy
+++ src/test/groovy/com/softtek/meetup/unit/RecoveryServiceSpec.groovy
@@ -4,8 +4,11 @@ import spock.lang.Specification
 
 import com.softtek.meetup.service.RecoveryService
 import com.softtek.meetup.service.RestService
+import com.softtek.meetup.service.RegistrationService
 import com.softtek.meetup.service.impl.RecoveryServiceImpl
 import com.softtek.meetup.repository.RegistrationCodeRepository
+import com.softtek.meetup.repository.UserRepository
+import com.softtek.meetup.model.User
 import com.softtek.meetup.model.RegistrationCode
 import com.softtek.meetup.command.Command
 
@@ -15,10 +18,14 @@ class RecoveryServiceSpec extends Specification {
 
   RegistrationCodeRepository repository = Mock(RegistrationCodeRepository)
   RestService restService = Mock(RestService)
+  RegistrationService registrationService = Mock(RegistrationService)
+  UserRepository userRepository = Mock(UserRepository)
 
   def setup(){
     recoveryService.repository = repository
     recoveryService.restService = restService
+    recoveryService.registrationService = registrationService
+    recoveryService.userRepository = userRepository
   }
 
 	void "should persist an registration code"(){
@@ -31,4 +38,19 @@ class RecoveryServiceSpec extends Specification {
     1 * restService.sendCommand(_ as Command)  
   }
 	
+  void "should confirm account for token"(){
+    given:"A token"
+      String token = 'token'
+    and:"An email"
+      String email = 'josdem@email.com'  
+    and:"A user"
+      User user = new User()  
+    when:"We call for account confirmation"
+      userRepository.findByEmail(email) >> user
+      registrationService.findEmailByToken(token) >> email 
+      User result = recoveryService.confirmAccountForToken(token) 
+    then:"We expect user is enabled"
+      result.enabled
+      1 * userRepository.save(user)
+  }
 }
