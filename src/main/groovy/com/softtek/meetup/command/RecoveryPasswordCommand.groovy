807c1b6ce124ea47c29455fc9a5861c4f6f8ce58
gabriel-fatih <gabriel.fatih@gmail.com>
2017-07-05 20:46:58 -0500

    #47 adding imports

--- src/main/groovy/com/softtek/meetup/command/RecoveryPasswordCommand.groovy
+++ src/main/groovy/com/softtek/meetup/command/RecoveryPasswordCommand.groovy
@@ -1,5 +1,9 @@
 package com.softtek.meetup.command
 
+import javax.validation.constraints.Size
+import javax.validation.constraints.NotNull
+import org.hibernate.validator.constraints.Email
+
 class RecoveryPasswordCommand implements Command {
 
   @Email
--- src/main/groovy/com/softtek/meetup/controller/RecoveryController.groovy
+++ src/main/groovy/com/softtek/meetup/controller/RecoveryController.groovy
@@ -1,12 +1,15 @@
 package com.softtek.meetup.controller
 
 import static org.springframework.web.bind.annotation.RequestMethod.GET
+import static org.springframework.web.bind.annotation.RequestMethod.POST
 
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.PathVariable
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.web.servlet.ModelAndView
 import org.springframework.stereotype.Controller
+import org.springframework.web.bind.annotation.InitBinder
+import org.springframework.web.bind.WebDataBinder
 
 import com.softtek.meetup.command.RecoveryPasswordCommand
 import com.softtek.meetup.service.RecoveryService
@@ -23,7 +26,7 @@ class RecoveryController {
 
   Logger log = LoggerFactory.getLogger(this.class)
   
-   @InitBinder('getEmailForRecovery')
+   @InitBinder('recoveryPasswordCommand')
 	private void initGetEmailBinder(WebDataBinder binder) {
 		binder.addValidators(generateRegistrationCodeForEmail)
     }
@@ -47,9 +50,10 @@ class RecoveryController {
 	ModelAndView getEmailForRecovery(@Valid RecoveryPasswordCommand command, BindingResult bindingResult){
 	
 	if(bindingResult.hasErrors()){
-      return new ModelAndView('home/home')
+      return new ModelAndView('recovery/password')
     }
     ModelAndView modelAndView = new ModelAndView('home/home')
     recoveryService.generateRegistrationCodeForEmail(command.email)
+    modelAndView
     }
 }
