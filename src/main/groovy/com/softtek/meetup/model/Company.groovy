785807a6e8cbd5db20e37132df5153021bda5253
josdem <joseluis.delacruz@gmail.com>
2017-08-01 20:01:33 -0500

    #65 Adding company entity

---
+++ src/main/groovy/com/softtek/meetup/model/Company.groovy
@@ -0,0 +1,22 @@
+package com.softtek.meetup.model
+
+import static javax.persistence.GenerationType.AUTO
+
+import java.io.Serializable
+import javax.persistence.Id
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+
+@Entity
+class Company implements Serializable {
+
+  @Id
+  @GeneratedValue(strategy=AUTO)
+  Long id
+
+  @Column(unique = true, nullable = false)
+  String name
+
+}
+
