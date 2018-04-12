214ebb39d80b0f2dc567b73959cfada40071bd38
josdem <joseluis.delacruz@gmail.com>
2018-04-12 15:00:51 -0500

    #98 Adding user controller

---
+++ src/main/java/com/softtek/meetup/controller/UserController.java
@@ -0,0 +1,56 @@
+package com.softtek.meetup.controller;
+
+import static org.springframework.web.bind.annotation.RequestMethod.GET;
+import static org.springframework.web.bind.annotation.RequestMethod.POST;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.InitBinder;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.validation.BindingResult;
+import org.springframework.stereotype.Controller;
+import javax.validation.Valid;
+
+import com.softtek.meetup.command.UserCommand;
+import com.softtek.meetup.validator.UserValidator;
+import com.softtek.meetup.service.UserService;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Controller
+@RequestMapping("/users")
+public class UserController {
+
+  @Autowired
+  private UserValidator userValidator;
+  @Autowired
+  private UserService userService;
+
+	private Logger log = LoggerFactory.getLogger(this.getClass());
+
+  @InitBinder
+  private void initBinder(WebDataBinder binder) {
+    binder.addValidators(userValidator);
+  }
+
+  @RequestMapping(method = GET, value = "/create")  
+  public String create(Model model){
+    model.addAttribute("userCommand", new UserCommand());
+    return "user/create";
+  }
+
+  @RequestMapping(method = POST)
+  public String save(@Valid UserCommand command, BindingResult bindingResult) {
+    log.info("Saving user:" + command.getUsername());
+
+    if (bindingResult.hasErrors()) {
+	    return "user/create";
+    }
+
+    userService.save(command);
+    return "redirect:/";
+  }
+
+}
