f8378056404578825394d2a66d52d63e97467c4f
josdem <joseluis.delacruz@gmail.com>
2018-04-12 11:53:49 -0500

    #96 Adding user repository

---
+++ src/main/groovy/com/softtek/meetup/repository/UserRepository.java
@@ -0,0 +1,12 @@
+package com.softtek.meetup.repository;
+
+import com.softtek.meetup.model.User;
+
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import reactor.core.publisher.Mono;
+
+public interface UserRepository extends ReactiveMongoRepository<User, String> {
+  Mono<UserDetails> findByUsername(String username);
+}
