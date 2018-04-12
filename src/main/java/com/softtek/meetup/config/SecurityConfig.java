9d453d7794233af88698494be5b3df84ab2230db
josdem <joseluis.delacruz@gmail.com>
2018-04-12 13:44:02 -0500

    #98 Adding user model

--- src/main/java/com/softtek/meetup/config/SecurityConfig.java
+++ src/main/java/com/softtek/meetup/config/SecurityConfig.java
@@ -1,12 +1,13 @@
 package com.softtek.meetup.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.userdetails.UserDetailsService;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
 import org.springframework.security.core.userdetails.ReactiveUserDetailsService;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import com.softtek.meetup.repository.UserRepository;
 
 @EnableWebFluxSecurity
 class SecurityConfig extends WebSecurityConfigurerAdapter {
