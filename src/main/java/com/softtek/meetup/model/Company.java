35a7f07b0b4d16b1ee4ba777a18d0afe950083cf
josdem <joseluis.delacruz@gmail.com>
2018-04-14 19:52:52 -0500

    #99 Changing company model from entity to data

---
+++ src/main/java/com/softtek/meetup/model/Company.java
@@ -0,0 +1,18 @@
+package com.softtek.meetup.model;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import lombok.Data;
+import lombok.ToString;
+
+@Data
+@Document
+@ToString
+public class Company {
+
+  @Id
+  private String uuid;
+  private String name;
+
+}
