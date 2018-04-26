42913ed92e79375bb885efa2c9ca52332d5a3be7
josdem <joseluis.delacruz@gmail.com>
2018-04-26 15:23:14 -0500

    #101 Creating user service interface and implementation

---
+++ src/main/java/com/softtek/meetup/service/UserService.java
@@ -0,0 +1,12 @@
+package com.softtek.meetup.service;
+
+import  com.softtek.meetup.model.User;
+import  com.softtek.meetup.command.Command;
+
+public interface UserService {
+
+  User getByUsername(String username);
+  User getByEmail(String email);
+  User save(Command command);
+
+}
---
+++ src/main/java/com/softtek/meetup/service/impl/UserServiceImpl.java
@@ -0,0 +1,38 @@
+package com.softtek.meetup.service.impl;
+
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import  com.softtek.meetup.model.User;
+import  com.softtek.meetup.command.Command;
+import  com.softtek.meetup.binder.UserBinder;
+import  com.softtek.meetup.service.UserService;
+import  com.softtek.meetup.service.RecoveryService;
+import  com.softtek.meetup.repository.UserRepository;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+  @Autowired
+  private UserBinder userBinder;
+  @Autowired
+  private UserRepository userRepository;
+  @Autowired
+  private RecoveryService recoveryService;
+
+  public User getByUsername(String username){
+    return userRepository.findByUsername(username);
+  }
+
+  public User getByEmail(String email){
+    return userRepository.findByEmail(email);
+  }
+
+  public User save(Command command){
+    User user = userBinder.bindUser(command);
+    userRepository.save(user);
+    recoveryService.sendConfirmationAccountToken(command.email);
+    return user;
+  }
+
+}
